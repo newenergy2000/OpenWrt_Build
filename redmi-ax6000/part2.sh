f93fd59b5aa1d98d85d9ea3a50bdf3e6320141b6
newenergy2000 <52132223+newenergy2000@users.noreply.github.com>
2022-11-13 23:01:03 +0800

    Update part2.sh

--- redmi-ax6000/part2.sh
+++ redmi-ax6000/part2.sh
@@ -3,3 +3,6 @@
 # replace default theme to argon
 rm -rf feeds/luci/themes/luci-theme-bootstrap/
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci*/Makefile
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
